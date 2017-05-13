2974c0d08f1a391f2011aa4067c7c5461abda284
LBoyle <louis_boyle@hotmail.com>
2017-05-13 13:11:01 +0100

    created two more tests for GET cuisines

--- test/controllers/cuisines_spec.js
+++ test/controllers/cuisines_spec.js
@@ -6,11 +6,35 @@ describe('Cuisines controllers tests', () => {
   describe('GET /api/cuisines', () => {
 
     it('should return a 200 response', function(done) {
+      // this.skip();
       api.get('/api/cuisines')
       .set('Accept', 'application/json')
       .end((err, res) => {
         if(err) console.log(err);
         expect(res.status).to.eq(200);
+        done();
+      });
+    });
+
+    it('should return an JSON object', function(done) {
+      // this.skip();
+      api.get('/api/cuisines')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          if (err) console.log(err);
+          expect(res.headers['content-type'])
+          .to.be.eq('application/json; charset=utf-8');
+          done();
+        });
+    });
+
+    it('should return an array of objects', function(done) {
+      // this.skip();
+      api.get('/api/cuisines')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if(err) console.log(err);
+        expect(res.body).to.be.an('array');
         done();
       });
     });
