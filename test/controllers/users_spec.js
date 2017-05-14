994787954570ca70aa7c583e1ab60743a6fe484e
LBoyle <louis_boyle@hotmail.com>
2017-05-14 21:03:44 +0100

    tested users index and show routes

--- test/controllers/users_spec.js
+++ test/controllers/users_spec.js
@@ -2,7 +2,7 @@ const {api, expect} = require('../spec_helper');
 const User = require('../../models/user');
 
 describe('Users and Auth test block', () => {
-
+  let myToken;
   let gUser;
 
   beforeEach(done => {
@@ -21,7 +21,7 @@ describe('Users and Auth test block', () => {
         'passwordConfirmation': 'password'
       })
       .then(user => {
-        // console.log(user.username+' created');
+        gUser = user;
         done();
       });
   });
@@ -56,7 +56,7 @@ describe('Users and Auth test block', () => {
         });
     });
 
-  });
+  }); // end of POST /api/register block
 
   describe('POST /api/login', () => {
 
@@ -86,7 +86,82 @@ describe('Users and Auth test block', () => {
         .catch(done);
     });
 
-  });
+  }); // end of POST /api/login block
+
+  describe('GET /api/users', () => {
+
+    beforeEach(done => {
+      api.post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: gUser.email,
+          password: 'password'
+        })
+        .end((err, res) => {
+          myToken = res.body.token;
+          done();
+        });
+    });
+
+    it('should return an array of user objects with correct keys', function(done) {
+      // this.skip();
+      api.get('/api/users')
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer '+myToken)
+        .then(res => {
+          expect(res.body)
+          .to.be.an('array');
+          res.body.forEach(user => {
+            expect(user)
+            .to.have.all.keys([
+              'username',
+              'email',
+              '_id'
+            ]);
+          });
+          done();
+        })
+        .catch(done);
+
+    });
+
+  }); // end of GET /api/users block
+
+  describe('GET /api/users/:id', () => {
+
+    it('should return a single user object with correct keys', function(done) {
+      // this.skip();
+      console.log(gUser._id);
+      api.get(`/api/users/${gUser._id}`)
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer '+myToken)
+        .then(res => {
+          expect(res.body)
+          .to.have.all.keys([
+            'username',
+            'email',
+            '_id'
+          ]);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should not return a user is the id is wrong', function(done) {
+      // this.skip();
+      console.log(gUser._id);
+      api.get('/api/users/56cb91bdc3464f14678934ca')
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer '+myToken)
+        .then(res => {
+          expect(res.status)
+          .to.eq(404);
+          done();
+        })
+        .catch(done);
+    });
+
+  }); // end of GET /api/users/:id block
 
 });
 
