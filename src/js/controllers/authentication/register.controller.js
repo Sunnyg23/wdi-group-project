395670651392e7e2414bb9ee745aecab2d899193
DaveMitten <davidmitten88@gmail.com>
2017-05-14 14:57:05 +0100

    finished Register Controller

--- src/js/controllers/authentication/register.controller.js
+++ src/js/controllers/authentication/register.controller.js
@@ -1,8 +1,19 @@
 angular
-  .module('veganChef');
-  .controller('RegisterCtrl', RegisterCtrl);
+.module('veganChef')
+.controller('RegisterCtrl', RegisterCtrl);
 
-  RegisterCtrl.$inject = [];
-  function RegisterCtrl(){
+RegisterCtrl.$inject = ['User', 'CurrentUserService', '$state'];
+function RegisterCtrl(User, CurrentUserService, $state){
   const vm = this;
-  }
+
+  vm.register = () => {
+    User
+    .register(vm.user).$promise
+    .then(()  =>  {
+      CurrentUserService.getUser();
+      $state.go('usersIndex');
+    }, err => {
+      console.log(err);
+    });
+  };
+}
